c87f79dd288aee085aae07f25fa09bd1daf82337
Alex Kurihara <alex.kurihara@gmail.com>
2017-03-25 13:18:27 -0400

    create install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Get current directory (so this script can be run from anywhere)
+
+export DOTFILES_DIR EXTRA_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Bunch of symlinks
+
+ln -sv "$DOTFILES_DIR/.bash_profile" ~
+ln -sv "$DOTFILES_DIR/.vimrc" ~
+
+# Package managers and packages
+
+. "$DOTFILES_DIR/install/brew.sh"
---
+++ install/brew.sh
@@ -0,0 +1,10 @@
+# Install Homebrew
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+
+brew install git
+brew install python
+brew install the_silver_searcher
+brew install tmux
+
