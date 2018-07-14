b6a15be9bdd5742c84cc40e93da984473a5dc6b5
Alex Kurihara <alex.kurihara@gmail.com>
2018-07-13 23:10:43 -0400

    Add brew cask

--- install/brew.sh
+++ install/brew.sh
@@ -8,3 +8,14 @@ brew install python
 brew install the_silver_searcher
 brew install tmux
 
+
+# Install Homebrew Cask
+brew tap caskroom/cask
+brew tap caskroom/versions
+
+brew cask install iterm2
+brew cask install google-chrome
+brew cask install spotify
+brew cask install vlc
+brew cask install slack
+brew cask install spectacle
