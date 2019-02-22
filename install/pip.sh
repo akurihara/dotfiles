c22b3c8f1e37c2fe53653477d028cfd545e06833
Alex Kurihara <alex.kurihara@gmail.com>
2019-02-21 20:30:42 -0500

    Update pip.sh

--- install/pip.sh
+++ install/pip.sh
@@ -1,10 +1,14 @@
-# Install Pip
+# Install pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 
-# Install Pip packages
+# Install global pip packages
 pip install virtualenv
 pip install virtualenvwrapper
 pip install requests
 pip install flake8
+pip install ipython
 pip install isort
+pip install nose
+pip install nosecomplete
+pip install ujson
