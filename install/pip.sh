85f81b75c655722e64466f68db4344b7c7ff3a6b
Alex Kurihara <alex.kurihara@gmail.com>
2019-10-25 23:21:23 -0400

    update global python libraries

--- install/pip.sh
+++ install/pip.sh
@@ -3,12 +3,12 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 
 # Install global pip packages
-pip install virtualenv
-pip install virtualenvwrapper
+pip install ipdb
+pip install ipython
 pip install requests
 pip install flake8
-pip install ipython
 pip install isort
 pip install nose
 pip install nosecomplete
 pip install ujson
+pip install black
