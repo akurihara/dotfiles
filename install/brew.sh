fd7f1f8d754ba062b1307806f8718d36645f0d73
Alex Kurihara <alex.kurihara@gmail.com>
2019-10-25 23:15:30 -0400

    update brew cask list

--- install/brew.sh
+++ install/brew.sh
@@ -9,12 +9,15 @@ brew install python
 brew install the_silver_searcher
 brew install tmux
 brew install pyenv
-brew install pyenv-virtualenv
-brew install pyenv-virtualenvwrapper
+brew install pipenv
+brew install ctags
+brew install postgresql
+brew install redis
+brew install sqlite
 
 # Install Python versions
 pyenv install 2.7.15
-pyenv install 3.7.2
+pyenv install 3.7.1
 
 # Install Homebrew Cask
 brew tap caskroom/cask
@@ -22,9 +25,17 @@ brew tap caskroom/versions
 
 # Use Homebrew Cask to install GUI applications.
 brew cask install iterm2
+brew cask install 1password
 brew cask install google-chrome
 brew cask install spotify
 brew cask install vlc
 brew cask install slack
 brew cask install spectacle
 brew cask install avast
+brew cask install authy
+brew cask install sequel-pro
+brew cask install postman
+brew cask install psequel
+brew cask install gfxcardstatus
+brew cask install figma
+brew cask isntall steam
